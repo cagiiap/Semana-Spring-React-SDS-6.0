3db478a75f21af27f15fc15b61430f90804b7ded
cagiiap <carjuniorrj@hotmail.com>
2022-01-11 12:30:32 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <BrowserRouter>
+      <Navbar></Navbar>
+
+      <Routes>
+        
+        <Route path="/" element={<Listing/>}></Route>
+        <Route path="/form" element={<Form/>}>
+          <Route path=":movieId" element={<Form/>}></Route>
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
