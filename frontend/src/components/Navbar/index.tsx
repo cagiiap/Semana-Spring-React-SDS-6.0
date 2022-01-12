6f5dcc89b04e0a06bf7ff53633f600229de8222a
cagiiap <carjuniorrj@hotmail.com>
2022-01-12 15:23:39 -0300

    add noreferrer on navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
 
-                    <a href="https://github.com/cagiiap" target="_blank">
+                    <a href="https://github.com/cagiiap" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/devsuperior</p>
